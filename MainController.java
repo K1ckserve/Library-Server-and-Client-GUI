74aff48bff58a18d90538c85b06c491e850fc6ab
Benjamin Dalton <bendalton6463@gmail.com>
2024-04-26 09:59:07 -0500

    Update MainController.java

--- MainController.java
+++ MainController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class MainController implements CatalogUpdateListener {
     @FXML
-    private Button Return;
+    private Button Logout1;
     @FXML
     private Button rent;
     @FXML
