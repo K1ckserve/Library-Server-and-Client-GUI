ec26757595515b42d05bd92b068e182b162c0757
Benjamin Dalton <bendalton6463@gmail.com>
2024-04-28 14:03:08 -0500

    starting JUNIT

---
+++ Serverside/libraryServerTest.java
@@ -0,0 +1,29 @@
+package Serverside;
+
+import static org.junit.Assert.*;
+public class libraryServerTest {
+
+    @org.junit.Test
+    public void main() {
+    }
+
+    @org.junit.Test
+    public void createUser() {
+    }
+
+    @org.junit.Test
+    public void serilizeResetPass() {
+    }
+
+    @org.junit.Test
+    public void sendCatalog() {
+    }
+
+    @org.junit.Test
+    public void sendUserCatalog() {
+    }
+
+    @org.junit.Test
+    public void sendAObject() {
+    }
+}
