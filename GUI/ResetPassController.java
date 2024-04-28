6ad990af465946a123ad1abf1a7a8aad28d09921
Benjamin Dalton <bendalton6463@gmail.com>
2024-04-27 21:59:15 -0500

    Update ResetPassController.java

--- GUI/ResetPassController.java
+++ GUI/ResetPassController.java
@@ -6,4 +6,30 @@ import javafx.scene.control.Button;
 public class ResetPassController {
     @FXML
     private Button Password;
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+    private libraryClient client
+    private void initialize(libraryClient client){
+        this.client = client;
+    }
+    protected void resetPassword(ActionEvent event){
+        String username.getText();
+        String password.getText();
+        client.resetPassword(username, password);
+        URL url = Paths.get("./GUI/Login.fxml").toUri().toURL();
+        FXMLLoader loader = new FXMLLoader(url);
+        loader.setLocation(url);
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Client");
+        primaryStage.show();
+    }
+
+
 }
